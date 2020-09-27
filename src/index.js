4df00f502a7e2f868c330a6cf07339a26f4928cb
Yurii Kovalchuk <yurii.kovalchuk@sisense.com>
2020-09-27 22:12:47 +0300

    add proxy config

--- src/index.js
+++ src/index.js
@@ -11,6 +11,18 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+console.log(process.env.REACT_APP_YURA);
+
+setTimeout(() => {
+  fetch('/api/test', {
+    method: 'GET'
+  })
+      .then(async (res) => {
+        const data = await res.json();
+        console.log(data);
+      });
+}, 3000);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
